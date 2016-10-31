3e4bafcdc9960057201a2edb57685e4759702c2b
Josh Farneman <josh.farneman@daveramsey.com>
2016-10-30 20:47:26 -0500

    Refactors to reduce indirection

--- src/index.js
+++ src/index.js
@@ -4,29 +4,13 @@ import jQuery from 'jquery';
 
 import template from './micro_template';
 
-const applyParamsToTemplate = (urlTemplate, params) => {
-  if (typeof urlTemplate !== 'function') {
-    return urlTemplate;
-  }
-
-  return urlTemplate(params);
-};
-
-const templateUrl = (url) => {
-  const templateDelimiter = /\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/;
-
-  if (!templateDelimiter.test(url)) {
-    return url;
-  }
-
-  return template(url);
-};
+const templateDelimiter = /\$\{([^\\}]*(?:\\.[^\\}]*)*)\}/;
 
 const immutableAssign = (...args) => Object.assign({}, ...args);
 
-const createRequester = (addFor, requestObject) => {
+const createRequester = (addFor, request) => {
   const requester = function (payload) {
-    const requestWithPayload = immutableAssign(requestObject, {data: payload});
+    const requestWithPayload = immutableAssign(request, {data: payload});
 
     if (typeof requestWithPayload.url === 'function') {
       throw new Error('Must supply url keys before calling fulfill');
@@ -36,44 +20,35 @@ const createRequester = (addFor, requestObject) => {
   };
 
   if (addFor) {
-    requester.for = (urlParams) => {
-      const appliedUrl = applyParamsToTemplate(requestObject.url, urlParams);
-      const requested = immutableAssign(requestObject, {url: appliedUrl});
-
-      return createRequester(false, requested);
-    };
+    requester.for = (urlParams) => createRequester(false, immutableAssign(request, {
+      url: typeof request.url === 'function' ? request.url(urlParams) : request.url
+    }));
   }
 
   return requester;
 };
 
-const configureRequestCreator = function (defaultRequest, requestList) {
-  return (requests, requestKey) => {
-    const request = requestList[requestKey];
+export default function abstrax(config: Object) {
+  const defaultConfig = immutableAssign({
+    method: 'GET',
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8'
+  }, config.defaults);
+
+  const requestCreator = (requests, requestKey) => {
+    const request = config.requests[requestKey];
 
     if (!request.url) {
       return requests;
     }
 
-    const templatedRequest = immutableAssign(request, {url: templateUrl(request.url)});
-    const preparedRequest = immutableAssign(defaultRequest, templatedRequest);
-
-    requests[requestKey] = createRequester(true, preparedRequest);
+    requests[requestKey] = createRequester(true, immutableAssign(defaultConfig, request, {
+      url: templateDelimiter.test(request.url) ? template(request.url) : request.url
+    }));
 
     return requests;
   };
-};
-
-export default function abstrax(config: Object) {
-  const defaultConfig = {
-    method: 'GET',
-    dataType: 'json',
-    contentType: 'application/json; charset=utf-8'
-  };
-
-  const defaultRequest = immutableAssign(defaultConfig, config.defaults);
-  const requestCreator = configureRequestCreator(defaultRequest, config.requests);
 
   return Object.keys(config.requests)
-        .reduce(requestCreator, {});
+    .reduce(requestCreator, {});
 }
